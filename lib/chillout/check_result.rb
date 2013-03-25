d2a4ef75ddd97fbb9cd7ab5078a274c628c4a97e
Jan Filipowski <jachuf@gmail.com>
2013-03-25 20:37:09 +0100

    CheckResult knows if there were authentication problems

--- lib/chillout/check_result.rb
+++ lib/chillout/check_result.rb
@@ -6,4 +6,8 @@ class CheckResult
   def successful?
     @response.respond_to?(:code) && @response.code == "200"
   end
+
+  def has_problem_with_authorization?
+    @response.respond_to?(:code) && @response.code == "401"
+  end
 end
--- test/check_result_test.rb
+++ test/check_result_test.rb
@@ -18,4 +18,28 @@ class CheckResultTest < ChilloutTestCase
     check_result = CheckResult.new(response)
     refute check_result.successful?
   end
+
+  def test_dont_have_problem_with_authorization_for_response_with_200_code
+    response = stub(code: "200")
+    check_result = CheckResult.new(response)
+    refute check_result.has_problem_with_authorization?
+  end
+
+  def test_have_problem_with_authorization_for_response_with_401_code
+    response = stub(code: "401")
+    check_result = CheckResult.new(response)
+    assert check_result.has_problem_with_authorization?
+  end
+
+  def test_dont_have_problem_with_authorization_for_response_with_other_code
+    response = stub(code: "501")
+    check_result = CheckResult.new(response)
+    refute check_result.has_problem_with_authorization?
+  end
+
+  def test_dont_have_problem_with_authorization_for_response_that_dont_respond_to_code
+    response = stub
+    check_result = CheckResult.new(response)
+    refute check_result.has_problem_with_authorization?
+  end
 end
