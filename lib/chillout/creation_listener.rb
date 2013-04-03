b383546f28d06da85c7e3d64517cc41c58de6a75
Jan Filipowski <jachuf@gmail.com>
2013-04-03 12:34:13 +0200

    Log about created models using Rails.logger

--- lib/chillout/creation_listener.rb
+++ lib/chillout/creation_listener.rb
@@ -6,6 +6,7 @@ module Chillout
       super
       class_name = subclass.name
       subclass.after_commit :on => :create do
+        Rails.logger.info "[Chillout] Model created: #{class_name}"
         Thread.current[:creations] ||= CreationsContainer.new
         creations = Thread.current[:creations]
         creations.increment!(class_name)
