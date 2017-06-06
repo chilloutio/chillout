3c9173b9c7dc48bd641abd83fdad0df347dce9f5
Robert Pankowecki <robert.pankowecki@gmail.com>
2017-06-06 10:17:17 +0200

    rails 4.2 app

---
+++ test/support/rails_4_2_0/app/models/entity.rb
@@ -0,0 +1,2 @@
+class Entity < ActiveRecord::Base
+end
---
+++ test/support/rails_4_2_0/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails400::Application.config.secret_key_base = '3be2015d889cbad431132564d247d7fa67b8edabf71065f59436ab184ebfbc41639c547ce573d9894707c09adf6acfa0b4af42c5df3ea8b4e822783ccccae6c6'
