3f9596619efc14466502c3c8c464f0c7c99471f0
Robert Pankowecki <robert.pankowecki@gmail.com>
2017-06-12 17:41:39 +0200

    Test can re-create worker thread

--- test/integration/client_test.rb
+++ test/integration/client_test.rb
@@ -33,4 +33,27 @@ class ClientIntegrationTest < ChilloutTestCase
     check_result = client.check_api_connection
     refute check_result.successful?, "Check was successful"
   end
+
+  def test_client_creats_new_worker_thread_after_exception
+    @_api_key = "xyz123"
+    stub_request(:post, api_url("clients")).with(basic_auth: ['xyz123', 'xyz123']).
+      to_return(:body => "OK", :status => 200)
+    stub_request(:post, api_url("metrics")).with(basic_auth: ['xyz123', 'xyz123']).
+      to_return(:body => "OK", :status => 200)
+
+    client = Chillout::Client.new(@_api_key)
+    client.enqueue(Chillout::CreationsContainer.new)
+    assert client.worker_running?
+
+    client.enqueue(nil)
+    sleep(6)
+    refute client.worker_running?
+
+    client.enqueue(Chillout::CreationsContainer.new)
+    assert client.worker_running?
+
+    client.enqueue(nil)
+    sleep(6)
+    refute client.worker_running?
+  end
 end
