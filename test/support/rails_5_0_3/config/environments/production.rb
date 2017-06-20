49d5b7b5ab369ccd9ea446aeb654cdafbd39b123
Robert Pankowecki <robert.pankowecki@gmail.com>
2017-06-20 09:30:25 +0200

    New config options

--- test/support/rails_5_0_3/config/environments/production.rb
+++ test/support/rails_5_0_3/config/environments/production.rb
@@ -20,7 +20,7 @@ Rails400::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+  config.public_file_server.enabled = false
 
 
   # Specifies the header that your server uses for sending files.
