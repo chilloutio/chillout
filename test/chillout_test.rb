649ce98eb7de25180d7a3d6fee33eba54f9dd9fb
Robert Pankowecki <robert.pankowecki@gmail.com>
2017-06-29 12:10:13 +0200

    Clean global state in tests

--- test/chillout_test.rb
+++ test/chillout_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class ChilloutModuleTest < ChilloutTestCase
+  def setup
+    Chillout.creations = nil
+  end
+
+  def teardown
+    Chillout.creations = nil
+  end
+
   def test_custom_metrics
     Chillout::Metric.track('RegistrationCompleted')
     assert_equal 1, Chillout.creations['RegistrationCompleted']
