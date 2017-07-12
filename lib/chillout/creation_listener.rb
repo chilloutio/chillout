52cb005ca1e84fc6ce6dc2ba31831846ac1b872f
Robert Pankowecki <robert.pankowecki@gmail.com>
2017-07-12 21:09:53 +0200

    Hotfix for anonymous class
    I managed to reproduce this bug with has_and_belongs_to_many association
    
    class Entity
      has_and_belongs_to_many :followers, ->{ distinct },
        class_name: "Entity",
        join_table: "entities_following_entities",
        foreign_key: "follower_id",
        association_foreign_key: "target_id"
    end
    
    To cause a bug run:
    
      entity.follower_ids=[1]
    
    (cherry picked from commit 604520fdf7b766c1092a6c3922cc6da021e5d4a4)

--- lib/chillout/creation_listener.rb
+++ lib/chillout/creation_listener.rb
@@ -10,6 +10,7 @@ module Chillout
     private
     def listen(monitored_class)
       class_name = monitored_class.name
+      return unless class_name
       monitored_class.after_commit :on => :create do
         Rails.logger.debug "[Chillout] Model created: #{class_name}"
         Chillout.creations ||= CreationsContainer.new
